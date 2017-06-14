7f7b16251c0b60933b972824c791811c42bdc84c
canaantt <canaantt@gmail.com>
2017-06-14 15:39:01 -0700

    use asyncloop to serialize the collection insertions

--- process.js
+++ process.js
@@ -8,44 +8,60 @@ var Pathway_To_ReactionLikeEvent = require("./data/Pathway_To_ReactionLikeEvent.
 var Pathways = require("./data/Pathway.json");
 var PathwayHierarchy = require("./data/PathwayHierarchy.json");
 var ReactionLikeEvent = require("./data/ReactionLikeEvent");
+var data = {
+    "PhysicalEntity": PhysicalEntity,
+    "ReactionLikeEvent_To_PhysicalEntity": ReactionLikeEvent_To_PhysicalEntity,
+    "Pathway_To_ReactionLikeEvent": Pathway_To_ReactionLikeEvent,
+    "Pathways": Pathways,
+    "PathwayHierarchy": PathwayHierarchy,
+    "ReactionLikeEvent": ReactionLikeEvent,
+};
 
-
+const asyncLoop = require('node-async-loop');
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/reactome-simple");
 var db = mongoose.connection;
 db.once("open", function(callback){
     console.log("Connection succeeded.");
-    db.collection("PhysicalEntity").insertMany(PhysicalEntity, function(err, result){
-                                                if (err) console.log(err);
-                                            }, function(){
-                                                console.log("PhysicalEntity insertion is completed.");
-                                            });
-    db.collection("ReactionLikeEvent_To_PhysicalEntity").insertMany(ReactionLikeEvent_To_PhysicalEntity, function(err, result){
-                                                if (err) console.log(err);
-                                            }, function(){
-                                                console.log("ReactionLikeEvent_To_PhysicalEntity insertion is completed.");
-                                            }); 
-    db.collection("Pathway_To_ReactionLikeEvent").insertMany(Pathway_To_ReactionLikeEvent, function(err, result){
-                                                if (err) console.log(err);
-                                            }, function(){
-                                                console.log("Pathway_To_ReactionLikeEvent insertion is completed.");
-                                            }); 
-    db.collection("Pathways").insertMany(Pathways, function(err, result){
-                                                if (err) console.log(err);
-                                            }, function(){
-                                                console.log("PhysicalEntity insertion is completed.");
-                                            }); 
-    db.collection("PathwayHierarchy").insertMany(PathwayHierarchy, function(err, result){
-                                                if (err) console.log(err);
-                                            }, function(){
-                                                console.log("PathwayHierarchy insertion is completed.");
-                                            });
-    db.collection("ReactionLikeEvent").insertMany(ReactionLikeEvent, function(err, result){
-                                                if (err) console.log(err);
-                                            }, function(){
-                                                console.log("ReactionLikeEvent insertion is completed.");
-                                            });                                                                                                                                                                                                         
+    asyncLoop(Object.keys(data), function(c, next){
+        console.log('Current Collection: ' + c);
+        db.collection(c).insertMany(data[c], function(err, result){
+            if (err) console.log(err);
+            next();
+        })
+    }, function(err){
+        if (err){
+            console.log('Error: ' + err.message);
+            return;
+        } else {
+            console.log("Finished!");
+            db.close();
+        }
+    });
 });
+// db.once("open", function(callback){
+//     console.log("Connection succeeded.");
+//     db.collection("PhysicalEntity").insertMany(PhysicalEntity, function(err, result){
+//                                                 if (err) console.log(err);
+//                                             });
+//     db.collection("ReactionLikeEvent_To_PhysicalEntity").insertMany(ReactionLikeEvent_To_PhysicalEntity, function(err, result){
+//                                                 if (err) console.log(err);
+//                                             }); 
+//     db.collection("Pathway_To_ReactionLikeEvent").insertMany(Pathway_To_ReactionLikeEvent, function(err, result){
+//                                                 if (err) console.log(err);
+//                                             }); 
+//     db.collection("Pathways").insertMany(Pathways, function(err, result){
+//                                                 if (err) console.log(err);
+//                                             }); 
+//     db.collection("PathwayHierarchy").insertMany(PathwayHierarchy, function(err, result){
+//                                                 if (err) console.log(err);
+//                                             });
+//     db.collection("ReactionLikeEvent").insertMany(ReactionLikeEvent, function(err, result){
+//                                                 if (err) console.log(err);
+//                                             });                                                                                                                                                                                                         
+// }, function(){
+//     console.log("completed.");
+// });
 
 
